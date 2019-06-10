3b9a35a26eb1f6ed0d881985adf6903a57235a74
Abdullah Alsaedi <altorgomy@hotmail.com>
2019-06-10 09:45:52 +0300

    Adding Hello Comment

--- MyConsoleApp/MyConsoleApp/Program.cs
+++ MyConsoleApp/MyConsoleApp/Program.cs
@@ -10,6 +10,9 @@ namespace MyConsoleApp
     {
         static void Main(string[] args)
         {
+            //Hello from Vs 2017
+
+
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
