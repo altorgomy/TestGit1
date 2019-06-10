167140ee75057698c8da9c1bb9c1a87f81b04d02
Abdullah Alsaedi <altorgomy@hotmail.com>
2019-06-10 09:51:29 +0300

    Thanks

--- MyConsoleApp/MyConsoleApp/Program.cs
+++ MyConsoleApp/MyConsoleApp/Program.cs
@@ -14,6 +14,9 @@ namespace MyConsoleApp
             
             //Hello from GitHub Repo
 
+            //Thanks a lot
+
+
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
